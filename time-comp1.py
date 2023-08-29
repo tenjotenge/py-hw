cad38521afe1c6145c541a26d355afc878f26856
nayatrov <nayatrov@outlook.com>
2023-08-29 15:08:22 -0400

    update

--- time-comp1.py
+++ time-comp1.py
@@ -1,1 +1,16 @@
 import time 
+
+
+def sum_int_1 (n):
+    sum = 0
+    for i in range (n+1) :
+        sum += 1
+    return sum
+
+def sum_int_2 (n) :
+    sum = 0
+    i = 0
+    while i < n + 1:
+        sum += 1
+        i += 1
+    return sum
