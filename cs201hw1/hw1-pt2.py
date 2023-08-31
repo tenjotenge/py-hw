0ce6a8326c8c3641bc2debbc38f6e67bcc7f7b72
nayatrov <nayatrov@outlook.com>
2023-08-31 14:52:37 -0400

    pt2 finished

--- cs201hw1/hw1-pt2.py
+++ cs201hw1/hw1-pt2.py
@@ -1,14 +1,21 @@
-def sum_int_1 (n):
-    sum = 0
-    for i in range (n+1) :
-        sum += 1
-    return sum
+def constant_summation(numbers):
+    n = len(numbers)
+    total_sum = (n * (n + 1)) // 2
+    return total_sum
 
-def sum_int_2 (n):
-    sum = 0
-    i = 0
-    while i < n + 1:
-        sum += 1
-        i += 1
-    return sum
+numbers = [1, 2, 3, 4, 5]
+result = constant_summation(numbers)
+print("Adding numbers from 1 to 5.")
+print("Constant Time: ")
+print(result)  # Output: 15 (1 + 2 + 3 + 4 + 5)
 
+def linear_summation(numbers):
+    total_sum = 0
+    for num in numbers:
+        total_sum += num
+    return total_sum
+
+numbers = [1, 2, 3, 4, 5]
+result = linear_summation(numbers)
+print("Linear Time: ")  # Output: 15 (1 + 2 + 3 + 4 + 5)
+print(result)
