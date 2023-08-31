a6e60fe37249ffb6d04cd83df8d3c9391d77d234
nayatrov <nayatrov@outlook.com>
2023-08-31 14:39:43 -0400

    pt2

---
+++ cs201hw1/hw1-pt2.py
@@ -0,0 +1,14 @@
+def sum_int_1 (n):
+    sum = 0
+    for i in range (n+1) :
+        sum += 1
+    return sum
+
+def sum_int_2 (n):
+    sum = 0
+    i = 0
+    while i < n + 1:
+        sum += 1
+        i += 1
+    return sum
+
